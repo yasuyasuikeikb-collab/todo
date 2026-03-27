5a096f23b553749adbadb7ab8f97d76f9c23d980
ikegami yasuki <yasuyasuikeikb@gmail.com>
2026-03-28 00:23:30 +0900

    完成

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Todo;
 use App\Http\Requests\TodoRequest;
+use Illuminate\Http\Request;
 
 class TodoController extends Controller
 {
@@ -20,4 +21,17 @@ class TodoController extends Controller
       return redirect('/')->with('success', 'Todoを作成しました');
     }
 
+    public function update(TodoRequest $request)
+    {
+        $todo = $request->only(['content']);
+        Todo::find($request->id)->update($todo);
+
+        return redirect('/')->with('success', 'Todoを更新しました');
+    }
+
+    public function destroy(Request $request)
+    {
+        Todo::find($request->id)->delete();
+        return redirect('/')->with('success', 'Todoを削除しました');  
+    }
 }
--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -41,9 +41,12 @@
         @foreach ($todos as $todo)
         <tr class="todo-table__row">
           <td class="todo-table__item">
-            <form class="update-form">
+            <form class="update-form" action="/todos/update" method="post">
+              @method('PATCH')
+              @csrf
               <div class="update-form__item">
                 <input class="update-form__item--input" type="text" name="content" value="{{ $todo->content }}">
+                <input type="hidden" name="id" value="{{ $todo->id }}">
               </div>
               <div class="update-form__button">
                 <button class="update-form__button--submit" type="submit">更新</button>
@@ -51,7 +54,10 @@
             </form>
           </td>
           <td class="todo-table__item">
-            <form class="delete-form">
+            <form class="delete-form" action="/todos/delete" method="post">
+              @method('DELETE')
+              @csrf
+              <input type="hidden" name="id" value="{{ $todo->id }}">
               <button class="delete-form__button--submit" type="submit">削除</button>
             </form>
           </td>
